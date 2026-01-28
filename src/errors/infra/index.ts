8ca87628befcdae2140d1fd53ed116b9239eecf7
Alan Reis <alanreisanjo@gmail.com>
2026-01-28 12:39:13 -0300

    Adição de novos erros de infra

--- src/errors/infra/index.ts
+++ src/errors/infra/index.ts
@@ -1,3 +1,4 @@
 export * from "./conflict.exception";
 export * from "./operation-not-allowed.exception";
 export * from "./database-unavailable.exception";
+export * from "./resource-not-found.exception";
